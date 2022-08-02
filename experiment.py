e77fefb9cf5fcc570a8f7aa0b71f1b710e671e1a
Yuchong Zhang <yuchongz@outlook.com>
2022-08-02 00:56:34 -0400

    simple experiment

---
+++ experiment.py
@@ -0,0 +1,94 @@
+from math import ceil
+import torch
+
+
+class Experiment():
+    def __init__(self, model):
+        self.model = model
+    
+    def set_up_exp(self):
+        pass
+
+    def run(self):
+        pass
+
+
+class SparseBenchmarkClassifier(Experiment):
+    def __init__(self, model):
+        super().__init__(model)
+    
+    
+    def set_up_exp(self, args, trainloader, testloader, optimizer, criterion):
+        self.trainloader = trainloader
+        self.testloader = testloader
+        self.optimizer = optimizer
+        self.epochs = args.epochs
+        self.criterion = criterion
+        self.sparsity_level = args.sparsity_level
+    
+    
+    def train_epoch(self):
+        self.model.train()
+        for batch_idx, (inputs, targets) in enumerate(self.trainloader):
+            outputs = self.model(inputs)
+            loss = self.criterion(outputs, targets)
+            loss.backwards()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+    
+    
+    def test_accuracy(self):
+        self.model.eval()
+        correct_pred = 0
+        for batch_idx, (inputs, targets) in enumerate(self.testloader):
+            outputs = self.model(inputs)
+            predictions = outputs.argmax(
+                dim=1, keepdim=True
+            )
+            correct_pred += predictions.eq(targets.view_as(predictions)).sum().item()
+        
+        return correct_pred / len(self.testloader.dataset)            
+            
+    
+    def sparsify(self):
+        """
+        sparse_level is a positive real number between 0 to 1.
+        components whose norm fall into the top sparse_level percentile will be kept,
+        and the rest components will be set to zero.
+        """
+        for param in self.model.parameters():
+            p_flat = param.flatten()
+            k = ceil(len(p_flat) * self.sparsity_level)
+            topk_vals, topk_inds = torch.topk(input=torch.abs(p_flat), k=k)
+            mask = torch.zeros(size=p_flat.shape, device=topk_inds.device)
+            mask.scatter_(0, topk_inds, 1, reduce='add')
+            param *= mask.reshape(param.size())
+    
+    
+    def reset_model(self):
+        for layer in self.model.children():
+            if hasattr(layer, 'reset_parameters'):
+                layer.reset_parameters()
+
+    
+    def run(self):
+        interm_sparsity_level = self.sparsity_level ** (1/self.epochs)
+        sparse_accuracies = []
+        dense_accuracies = []
+        for _ in range(1, self.epochs + 1):
+            self.sparsify(interm_sparsity_level)
+            self.train_epoch(self.trainloader, self.optimizer, self.criterion)
+            sparse_accuracies.append(self.test_accuracy(self.testloader))
+            interm_sparsity_level *= self.sparsity_level
+        
+        self.reset_model()
+        
+        for _ in range(1, self.epochs + 1):
+            self.train_epoch(self.trainloader, self.optimizer, self.criterion)
+            dense_accuracies.append(self.test_accuracy(self.testloader))
+        
+        return sparse_accuracies, dense_accuracies
+        
+
+        
+    
---
+++ main.py
@@ -0,0 +1,58 @@
+from cProfile import label
+from cmath import exp
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import argparse
+import torchvision.transforms as transforms
+import models
+from experiment import *
+import torch.nn as nn
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="PyTorch CIFAR10 DP Training")
+
+    parser.add_argument("--epochs", default=1, type=int,)
+    parser.add_argument("--batch_size", default=100, type=int)
+    parser.add_argument("--sample-rate", default=0.04, type=float)
+    parser.add_argument("--lr", default=4e-2, type=float)
+    parser.add_argument("--momentum", default=0.9, type=float)
+    parser.add_argument("--sparsity_level", default=0.1, type=float)
+    
+    args = parser.parse_args()
+    
+    transform = transforms.Compose(
+    [
+        transforms.ToTensor()
+    ])
+    trainset = torchvision.datasets.CIFAR10(root='./datasets', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+                                            shuffle=True)
+
+    testset = torchvision.datasets.CIFAR10(root='./datasets', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
+                                            shuffle=False)
+
+    USE_CUDA = torch.cuda.is_available()
+    device = torch.device("cuda" if USE_CUDA else "cpu")
+    print(f'training device: {device}')
+    print(f'training set size: {len(trainset)}')
+    
+    model = models.LeNet5(num_classes=10)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lr)
+    criterion = nn.CrossEntropyLoss()
+    experiment = SparseBenchmarkClassifier(model)
+    experiment.set_up_exp(trainloader, testloader, optimizer, criterion)
+    sparse_accuracies, dense_accuracies = experiment.run()
+    
+    epochs_lst = [_ for _ in range(1, args.epochs + 1)]
+    plt.plot(epochs_lst, sparse_accuracies, label='sparse')
+    plt.plot(epochs_lst, dense_accuracies, label='dense')
+    plt.legend(loc="lower center", bbox_to_anchor=(0.8, 0.25))
+    plt.savefig('./sparse_acc_plot')
+    
+    
+
+    
